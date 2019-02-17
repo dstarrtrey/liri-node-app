9c849fa8c0c7aeb59f69bcfc43ec9aa51a19221c
David Starr <dstarrtrey@gmail.com>
2019-02-17 14:44:33 -0800

    Adds concert-this functionality

--- liri.js
+++ liri.js
@@ -1,8 +1,31 @@
 require("dotenv").config();
 const keys = require("./keys.js");
-const spotify = new Spotify(keys.spotify);
+//const spotify = new Spotify(keys.spotify);
 const axios = require("axios");
 const chalk = require("chalk");
+const moment = require("moment");
+const concertThis = async () => {
+  if (process.argv[4]){
+    console.log(chalk.bgRed('Try entering the artist with quotes ("") surrounding it.'));
+    return;
+  }
+  console.log(chalk.bgCyan(process.argv[3]));
+  const artist = process.argv[3].split(" ").join("+");
+  const artistUrl = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`
+  try{
+    let response = await axios.get(artistUrl);
+    const data = response.data;
+    data.forEach(concert => {
+      console.log(chalk.yellow("Venue:"), concert.venue.name);
+      console.log(chalk.blue("Location:"), concert.venue.city + ", " + concert.venue.region);
+      console.log(chalk.blue("Date:"), moment(concert.datetime).format("MM/DD/YYYY"));
+    });
+  }
+  catch(err){
+    console.log("This is an error message: ");
+    console.log(err)
+  }
+};
 switch (process.argv[2]) {
   case "concert-this": 
     concertThis();
@@ -15,10 +38,3 @@ switch (process.argv[2]) {
     break;
 }
 
-const concertThis = async () => {
-  const artist = process.argv[3].split(" ").join("+");
-  let data = await axios.get(`https://rest.bandsintown.com/artists/"${artist}/events?app_id=codingbootcamp`);
-  data.forEach(concert => {
-    console.log()
-  });
-};
