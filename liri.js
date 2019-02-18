d1c75bc4d875b92886dc28f0ecece0544218af18
David Starr <dstarrtrey@gmail.com>
2019-02-18 14:57:36 -0800

    Adds all functionality

--- liri.js
+++ liri.js
@@ -7,59 +7,115 @@ const chalk = require("chalk");
 const moment = require("moment");
 const fs = require("fs");
 const concertThis = async (band = process.argv[3]) => {
-  if (process.argv[4]){
-    console.log(chalk.bgRed('Try entering the artist with quotes ("") surrounding it.'));
+  if (process.argv[4]) {
+    console.log(
+      chalk.bgRed('Try entering the artist with quotes ("") surrounding it.')
+    );
     return;
   }
   console.log(chalk.bgCyan(band));
-  const artist = band.split(" ").join("+").trim().replace(/['"]+/g, '');
-  const artistUrl = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`
+  fs.appendFile(
+    "log.txt",
+    band,
+    err => {
+      if (err) throw err;
+    }
+  );
+  const artist = band
+    .split(" ")
+    .join("+")
+    .trim()
+    .replace(/['"]+/g, "");
+  const artistUrl = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
   try {
     let response = await axios.get(artistUrl);
     response.data.forEach(concert => {
       console.log(chalk.yellow("Venue:"), concert.venue.name);
-      console.log(chalk.blue("Location:"), concert.venue.city + ", " + concert.venue.region);
-      console.log(chalk.blue("Date:"), moment(concert.datetime).format("MM/DD/YYYY"));
+      console.log(
+        chalk.blue("Location:"),
+        concert.venue.city + ", " + concert.venue.region
+      );
+      console.log(
+        chalk.blue("Date:"),
+        moment(concert.datetime).format("MM/DD/YYYY")
+      );
+      fs.appendFile(
+        "log.txt",
+        `| Venue - ${concert.venue.name}, Location - ${
+          concert.venue.city + ", " + concert.venue.region
+        }, Date - ${moment(concert.datetime).format("MM/DD/YYYY")}`,
+        err => {
+          if (err) throw err;
+        }
+      );
     });
-  }
-  catch(err){
+    fs.appendFile(
+      "log.txt",
+      "]",
+      err => {
+        if (err) throw err;
+      }
+    );
+  } catch (err) {
     console.log("This is an error message: ");
-    console.log(err)
+    console.log(err);
   }
 };
 const spotifyThisSong = async (song = process.argv[3]) => {
-  if (process.argv[4]){
-    console.log(chalk.bgRed('Try entering the song with quotes ("") surrounding it.'));
+  if (process.argv[4]) {
+    console.log(
+      chalk.bgRed('Try entering the song with quotes ("") surrounding it.')
+    );
     return;
   }
   if (!song) {
     song = "The Sign Ace of Base";
   }
-  spotify.search({ type: "track", query: song}, function(err, data) {
+  spotify.search({ type: "track", query: song }, function(err, data) {
     if (err) {
-      return console.log('Error occurred: ' + err);
-    };
+      return console.log("Error occurred: " + err);
+    }
     const song = data.tracks.items[0];
     console.log(chalk.blue("Song Name:"), song.name);
     console.log(chalk.yellow("Artist(s):"), song.artists[0].name);
-    console.log(chalk.magenta("Listen URL:"), chalk.bgCyan(song.external_urls.spotify));
-    console.log(chalk.green("Album:"),song.album.name);
+    console.log(
+      chalk.magenta("Listen URL:"),
+      chalk.bgCyan(song.external_urls.spotify)
+    );
+    console.log(chalk.green("Album:"), song.album.name);
+    fs.appendFile(
+      "log.txt",
+      `Song Name - ${song.name}, Artist(s) - ${
+        song.artists[0].name
+      }, Listen URL - ${song.external_urls.spotify}, Album - ${
+        song.album.name
+      }]`,
+      err => {
+        if (err) throw err;
+      }
+    );
   });
 };
 const movieThis = async (input = process.argv[3]) => {
-  if (process.argv[4]){
-    console.log(chalk.bgRed('Try entering the artist with quotes ("") surrounding it.'));
+  if (process.argv[4]) {
+    console.log(
+      chalk.bgRed('Try entering the artist with quotes ("") surrounding it.')
+    );
     return;
   }
-  let req = input.split(" ").join("+").trim();
+  let req = input
+    .split(" ")
+    .join("+")
+    .trim();
   if (!input) {
     req = "Mr.+Nobody";
   }
   const movieUrl = `http://www.omdbapi.com/?apikey=trilogy&t=${req}`;
-  try{
+  try {
     let movie = await axios.get(movieUrl);
     movie = movie.data;
     console.log(chalk.yellow("Movie:"), movie.Title);
+
     console.log(chalk.blue("Year:"), movie.Year);
     console.log(chalk.red("IMDB Rating:"), movie.Ratings[0].Value);
     console.log(chalk.red("Rotten Tomatoes Score:"), movie.Ratings[1].Value);
@@ -67,23 +123,35 @@ const movieThis = async (input = process.argv[3]) => {
     console.log(chalk.cyan("Language"), movie.Language);
     console.log(chalk.blue("Plot:"), movie.Plot);
     console.log(chalk.green("Actors:"), movie.Actors);
-  }
-  catch(err){
+    fs.appendFile(
+      "log.txt",
+      `Year - ${movie.Year}, IMDB - ${
+        movie.Ratings[0].Value
+      }, Rotten Tomatoes - ${movie.Ratings[1].value}, Country - ${
+        movie.Country
+      }, Language - ${movie.Language}, Plot - ${movie.Plot}, Actors ${
+        movie.Actors
+      }]`,
+      err => {
+        if (err) throw err;
+      }
+    );
+  } catch (err) {
     console.log("This is an error message: ");
-    console.log(err)
+    console.log(err);
   }
 };
 const doWhatItSays = () => {
   fs.readFile("./random.txt", "utf8", (err, data) => {
-    if(err) {
+    if (err) {
       console.log("ERROR!", err);
     }
     const result = data.split(",");
     switch (result[0]) {
-      case "concert-this": 
+      case "concert-this":
         concertThis(result[1]);
         break;
-      case "spotify-this-song": 
+      case "spotify-this-song":
         spotifyThisSong(result[1]);
         break;
       case "movie-this":
@@ -93,10 +161,10 @@ const doWhatItSays = () => {
   });
 };
 switch (process.argv[2]) {
-  case "concert-this": 
+  case "concert-this":
     concertThis();
     break;
-  case "spotify-this-song": 
+  case "spotify-this-song":
     spotifyThisSong();
     break;
   case "movie-this":
@@ -106,4 +174,10 @@ switch (process.argv[2]) {
     doWhatItSays();
     break;
 }
-
+fs.appendFile(
+  "log.txt",
+  "[" + process.argv[2] + " " + process.argv[3] + ": ",
+  err => {
+    if (err) throw err;
+  }
+);
